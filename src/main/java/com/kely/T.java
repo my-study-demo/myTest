84f2373fa924c54525ad92d07d335c048d479c4c
yangqinghua <qinghua.yang@lovelawu.com>
2018-03-16 16:59:04 +0800

    init

---
+++ src/main/java/com/kely/T.java
@@ -0,0 +1,27 @@
+package com.kely;
+
+public class T {
+    private int second;
+    private String score;
+
+    public T(int second, String score) {
+        this.second = second;
+        this.score = score;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+}
